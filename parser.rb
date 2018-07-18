5a12d1df74b9b4a116b83e15c1cbbc132fddc96b
Jeffrey Neil Crockett <jeffreyneilcrockett@gmail.com>
2018-07-18 02:14:39 -0400

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -1,19 +1,9 @@
 require 'rest-client'
-require 'json'
-
-def related_concepts_for(query)
-    #query should have underscores instead of spaces
-    RestClient.get("http://en.wikipedia.org/wiki/#{query}").body.scan(/<a href="\/wiki\/.+\">/).map do |x|
-        x.split("title=").last[1..-3]
+def all_links_for(query)
+    #query should have underscores instead of spaces, otherwise it will 404
+    #todo: handle query formatting. query is case-sensitive, and capitalize will not always work
+    RestClient.get("http://en.wikipedia.org/wiki/#{query}").body.scan(/<i><a href=".+<\/a><\/i>/).map{|x|x.split(' ')[1][7..-2]}
     end
 end
 
-def pruned_concepts_for(query)
-    #filters out entries from related_concepts_for(query) that have html markup
-    concepts = related_concepts_for(query)
-    concepts.reject do |concept|
-        concept.count('<') > 0
-    end
-end
-
-puts pruned_concepts_for('dog')
+puts all_links_for('Kevin Bacon')
