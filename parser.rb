4be657e393948c8e09f2d115f47523e58f09ddee
Jeffrey Neil Crockett <jeffreyneilcrockett@gmail.com>
2018-07-18 02:18:29 -0400

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 def all_links_for(query)
     #query should have underscores instead of spaces, otherwise it will 404
     #todo: handle query formatting. query is case-sensitive, and capitalize will not always work
-    RestClient.get("http://en.wikipedia.org/wiki/#{query}").body.scan(/<i><a href=".+<\/a><\/i>/).map{|x|x.split(' ')[1][7..-2]}
+    RestClient.get("http://en.wikipedia.org/wiki/#{query}").body.scan(/<i><a href=".+<\/a><\/i>/).map{|x|x.split(' ')[1][12..-2]}
     end
 end
 
